4d3fc4f058fd3663648403693f747f18ceae77c9
yueMrzhao <874956782@qq.com>
2019-07-28 09:45:34 +0800

    Update keepalived-config.sh

--- keepalived-config.sh
+++ keepalived-config.sh
@@ -4,7 +4,7 @@ global_defs {
 }
 
 vrrp_script CheckK8sMaster {
-    script "curl -k https://192.168.4.245:6443"
+    script "curl -k https://192.168.10.24:6443"
     interval 3
     timeout 9
     fall 2
@@ -17,18 +17,18 @@ vrrp_instance VI_1 {
     virtual_router_id 61
     priority 100
     advert_int 1
-    mcast_src_ip 192.168.4.240
+    mcast_src_ip 192.168.10.12
     nopreempt
     authentication {
         auth_type PASS
         auth_pass sqP05dQgMSlzrxHj
     }
     unicast_peer {
-       	192.168.4.241
-       	192.168.4.242
+       	192.168.10.13
+       	192.168.10.14
     }
     virtual_ipaddress {
-        192.168.4.245/24
+        192.168.10.24/24
     }
     track_script {
         CheckK8sMaster
